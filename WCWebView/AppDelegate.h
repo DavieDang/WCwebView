cda5e5f5250590ce37a47f67519766112ed52f83
dangwc <dangwc@gz-mstc.com>
2017-12-18 17:32:47 +0800

    Initial Commit

---
+++ WCWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCWebView
+//
+//  Created by dangwc on 2017/12/18.
+//  Copyright © 2017年 dangwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
