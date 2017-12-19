1094e6c0767904278491bcea64861139c62e01db
dangwc <dangwc@gz-mstc.com>
2017-12-19 16:49:11 +0800

    添加加载动画

---
+++ WCWebView/WCAlterView.h
@@ -0,0 +1,13 @@
+//
+//  WCAlterView.h
+//  WCWebView
+//
+//  Created by dangwc on 2017/12/19.
+//  Copyright © 2017年 dangwc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCAlterView : NSObject
+
+@end
---
+++ WCWebView/WCAlterView.m
@@ -0,0 +1,13 @@
+//
+//  WCAlterView.m
+//  WCWebView
+//
+//  Created by dangwc on 2017/12/19.
+//  Copyright © 2017年 dangwc. All rights reserved.
+//
+
+#import "WCAlterView.h"
+
+@implementation WCAlterView
+
+@end
